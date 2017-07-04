234bf5daa96517e6a7f63d35ced3094b779dde9a
Andrea Zucchini <azga84@gmail.com>
2017-07-05 01:14:20 +0200

    add event dispatcher

--- src/Infrastructure/Api/Review/ReviewController.ts
+++ src/Infrastructure/Api/Review/ReviewController.ts
@@ -4,15 +4,16 @@ import { UpdateReviewService } from '../../../Review/Application/UpdateReview/Up
 import { UpdateReviewResponse } from '../../../Review/Application/UpdateReview/UpdateReviewResponse'
 import { UpdateReviewCommand } from '../../../Review/Application/UpdateReview/UpdateReviewCommand'
 import { LoggerService } from '../../Persistence/Logger'
-
+import eventDispatcher from '../../../Core/EventDispatcher'
 
 export class ReviewController {
-  update (req: express.Request, res: express.Response) {
+  update (req: express.Request, res: express.Response): void {
     const reviewId: string = req.params.id
     const result: UpdateReviewResponse = new UpdateReviewService(
       new UpdateReviewResponse(),
       new InMemoryReviewRepository(),
-      new LoggerService()
+      new LoggerService(),
+      eventDispatcher
     ).perform(new UpdateReviewCommand(reviewId))
     res.status(result.getStatusCode()).json(result.toJSON())
   }
