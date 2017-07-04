22807c4bc6f7c31b8360baf66d42f293bf094fd3
Andrea Zucchini <azga84@gmail.com>
2017-07-05 01:14:39 +0200

    add global subscribers

--- src/Infrastructure/App.ts
+++ src/Infrastructure/App.ts
@@ -1,7 +1,11 @@
 import * as express from 'express'
-import {Express, Router, Request, Response} from 'express'
-import {ReviewRoutes} from './Api/Review/ReviewRoutes'
+import { Express, Router, Request, Response } from 'express'
+import { ReviewRoutes } from './Api/Review/ReviewRoutes'
 import { Connect } from './Middlewares/Connect'
+import eventDispatcher from '../Core/EventDispatcher'
+import { UpdateReviewCommit } from '../Review/Domain/UpdateReviewCommit'
+import { LoggerService } from './Persistence/Logger'
+
 
 class App {
   public express: Express
@@ -10,7 +14,9 @@ class App {
     this.express = express()
     this.mountMiddlewares()
     this.mountRoutes()
+    this.addGlobalSubscribers()
   }
+
   private mountRoutes (): void {
     this.router.get('/ping', (req: Request, res: Response) => {
       res.json({
@@ -21,8 +27,12 @@ class App {
     this.express.use(new ReviewRoutes().getRoutes())
   }
 
-  private mountMiddlewares () {
+  private mountMiddlewares (): void {
     this.express.use(new Connect().config())
+  }
+
+  private addGlobalSubscribers (): void {
+    eventDispatcher.subscribe(new UpdateReviewCommit(new LoggerService()))
   }
 }
 
